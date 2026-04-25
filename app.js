a3fa49801ef792e21c1a4ef0a1eec5f7afc74e19
sampurna017 <sampurnamarasini017@gmail.com>
2026-04-25 20:06:37 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 const player = document.getElementById("player");
-const watched = document.getElementById("watched");
 
 // Load YouTube video
 function loadVideo(videoId) {
   player.src = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-  
-  // increase count
-  watched.textContent = parseInt(watched.textContent) + 1;
 }
-
-// Dark mode toggle
-document.getElementById("themeToggle").addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
