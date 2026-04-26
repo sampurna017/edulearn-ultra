3a83bb9342220f3f3452a85ab42b130315ae0f3e
sampurna017 <sampurnamarasini017@gmail.com>
2026-04-26 08:38:10 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const player = document.getElementById("player");
 const scoreEl = document.getElementById("score");
-const titleEl = document.getElementById("videoTitle");
+const titleEl = document.getElementById("title");
 
-let score = localStorage.getItem("score") || 0;
-let watched = JSON.parse(localStorage.getItem("watched")) || [];
-
-scoreEl.textContent = score;
+let score = 0;
+let watched = new Set();
 
 function loadVideo(id, title) {
   player.src = `https://www.youtube.com/embed/${id}?autoplay=1`;
   titleEl.textContent = title;
 
-  if (!watched.includes(id)) {
-    watched.push(id);
-    score = parseInt(score) + 10;
-
-    localStorage.setItem("score", score);
-    localStorage.setItem("watched", JSON.stringify(watched));
-
+  if (!watched.has(id)) {
+    watched.add(id);
+    score += 10;
     scoreEl.textContent = score;
   }
 }
